ace458a85c5f470c88446483f4de4a0105526110
Squadtech <wwwpink44@gmail.com>
2021-09-24 21:11:15 +0500

    Initial commit

---
+++ app/src/main/java/com/jo/zmzm/sweets/MainActivity.java
@@ -0,0 +1,305 @@
+package com.jo.zmzm.sweets;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
+
+    private WebView mWebView;
+    private SwipeRefreshLayout swipeRefreshLayout;
+
+    GPSTracker gps;
+    double latitude = 0, longitude = 0;
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        mWebView = findViewById(R.id.myWebView);
+
+        //swipe refresh
+        swipeRefreshLayout = findViewById(R.id.swipe_refresh_layout);
+        swipeRefreshLayout.setOnRefreshListener(this);
+
+        gps = new GPSTracker(MainActivity.this);
+
+        // Check if GPS enabled
+        if(gps.canGetLocation()) {
+
+            latitude = gps.getLatitude();
+            longitude = gps.getLongitude();
+
+
+
+            // \n is for new line
+            Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+        } else {
+            // Can't get location.
+            // GPS or network is not enabled.
+            // Ask user to enable GPS/network in settings.
+            statusCheck();
+        }
+
+
+        //Web View
+
+        if (Build.VERSION.SDK_INT >= 19) {
+            mWebView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        }
+        else {
+            mWebView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        }
+
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        //improve WebView performance
+        mWebView.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
+        // For API level below 18 (This method was deprecated in API level 18)
+        mWebView.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
+
+        mWebView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+        if (Build.VERSION.SDK_INT >= 19) {
+            mWebView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        }
+        else {
+            mWebView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        }
+        //mWebView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        //mWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        //mWebView.clearCache(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+        webSettings.setUseWideViewPort(true);
+        //webSettings.setEnableSmoothTransition(true);
+
+
+
+        mWebView.loadUrl(SplashActivity.urlString);
+        //force links open in webview only
+        mWebView.setWebViewClient(new MyWebViewClient());
+
+
+    }
+
+    public void statusCheck() {
+        final LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            buildAlertMessageNoGps();
+        }
+    }
+
+    private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+                        dialog.cancel();
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    @Override
+    public void onRefresh() {
+        mWebView.reload();
+        swipeRefreshLayout.setRefreshing(false);
+        gps = new GPSTracker(MainActivity.this);
+        if(gps.canGetLocation()) {
+
+            latitude = gps.getLatitude();
+            longitude = gps.getLongitude();
+
+            // \n is for new line
+            mWebView.evaluateJavascript("javascript: document.getElementById('geo').textContent='"+latitude+", "+longitude+"';",value -> {});
+
+            //Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+
+
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+
+
+            Log.d(TAG, "shouldOverrideUrlLoading: " + url);
+
+            if (url.contains("get-location")) {
+                grantLocationPermission();
+
+                return true;
+            } else if (url.contains("external=1")) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+                return true;
+            } else if (url.startsWith("tel:")) {
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse(url));
+                startActivity(intent);
+                //view.reload();
+                return true;
+            } else if (url.startsWith("mailto:")) {
+                //Handle mail Urls
+                startActivity(new Intent(Intent.ACTION_SENDTO, Uri.parse(url)));
+                return true;
+            } else if (url.contains("maps")) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+                return true;
+            } else {
+                view.loadUrl(url);
+            }
+
+            return false;
+        }
+
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+
+
+            /*progressDialog = new ProgressDialog(Home.this);
+            progressDialog.setTitle("Please Wait...");
+            progressDialog.setMessage("Loading...");
+            progressDialog.show();
+            progressDialog.setCancelable(false);*/
+            //isConnected(Home.this);
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+            super.onReceivedError(view, errorCode, description, failingUrl);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+
+            /*if (progressDialog != null)
+            {
+                progressDialog.dismiss();
+            }*/
+            mWebView.evaluateJavascript("javascript: document.getElementById('geo').textContent='"+latitude+", "+longitude+"';",value -> {});
+
+            super.onPageFinished(view, url);
+        }
+    }
+
+    private void grantLocationPermission() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)){
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }else{
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        }else {
+            Toast.makeText(MainActivity.this, "Location" , Toast.LENGTH_SHORT).show();
+
+
+            mWebView.evaluateJavascript("javascript: document.getElementById('geo').textContent='"+latitude+", "+longitude+"';",null);
+
+
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(MainActivity.this,
+                            Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                        gps = new GPSTracker(MainActivity.this);
+                        if(gps.canGetLocation()) {
+
+                            latitude = gps.getLatitude();
+                            longitude = gps.getLongitude();
+
+                            // \n is for new line
+                            mWebView.evaluateJavascript("javascript: document.getElementById('geo').textContent='"+latitude+", "+longitude+"';",null);
+
+                            // Inject Locaion
+                        }
+
+                    }
+                } else {
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction()== KeyEvent.ACTION_DOWN)
+        {
+            switch (keyCode)
+            {
+                case KeyEvent.KEYCODE_BACK:
+                    if (mWebView!=null)
+                    {
+                        if (mWebView.canGoBack())
+                        {
+                            mWebView.goBack();
+                        }
+                        else
+                        {
+                            finish();
+                        }
+                        return true;
+                    }
+
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
---
+++ app/src/main/java/com/jo/zmzm/sweets/SplashActivity.java
@@ -0,0 +1,131 @@
+package com.jo.zmzm.sweets;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.util.Log;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.github.ybq.android.spinkit.style.ThreeBounce;
+import com.onesignal.OneSignal;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final String ONESIGNAL_APP_ID = "9a5b658c-0182-432d-931e-4fb2a8b2e03a";
+    boolean checkGpsOnResume = false;
+    private ProgressBar progressBar;
+    String user_id;
+    public static String urlString;
+
+    public static final String TAG = SplashActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //OneSignal
+        // Enable verbose OneSignal logging to debug issues if needed.
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
+
+        // OneSignal Initialization
+        OneSignal.initWithContext(this);
+        OneSignal.setAppId(ONESIGNAL_APP_ID);
+        user_id = OneSignal.getDeviceState().getUserId();
+
+
+        grantLocationPermission();
+
+        progressBar = findViewById(R.id.spinKitView);
+
+        ThreeBounce db = new ThreeBounce();
+
+        progressBar.setIndeterminateDrawable(db);
+
+
+    }
+
+    private void grantLocationPermission() {
+        if (ContextCompat.checkSelfPermission(SplashActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(SplashActivity.this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)){
+                ActivityCompat.requestPermissions(SplashActivity.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }else{
+                ActivityCompat.requestPermissions(SplashActivity.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        }else {
+            countDownTimer();
+        }
+    }
+
+    private void countDownTimer() {
+
+        new CountDownTimer(2000, 1000) {
+            @Override
+            public void onTick(long l) {
+
+            }
+
+            @Override
+            public void onFinish() {
+
+                //This thing was missing so I need to take care of this
+                user_id = OneSignal.getDeviceState().getUserId();
+                Log.d(TAG, "onFinish: " + user_id);
+                if (user_id == null) {
+                    countDownTimer();
+                } else {
+                        urlString = "https://zmzm.x10.ltd/mobile.php?device-id=" + user_id + "&os=android";
+                        Log.d(TAG, "Targer Url: " + urlString);
+                        //Toast.makeText(SplashActivity.this, "Targer Url: " + urlString, Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                        finish();
+                    }
+                }
+
+        }.start();
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(SplashActivity.this,
+                            Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                        countDownTimer();
+                    }
+                } else {
+                    countDownTimer();
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+    }
+}
